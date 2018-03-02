7877261b8df6001cbf8c3c1932d635dd255303f8
Andrew Archer <aarcher@slack-corp.com>
2018-03-02 15:34:56 -0800

    reconnect on websocket disconnect

--- public/js/utils/ws.js
+++ public/js/utils/ws.js
@@ -1,12 +1,25 @@
-import { wsEvent } from '../stores';
+import { wsEvent, store } from '../stores';
+import { getGameId } from '../stores/game-store';
 
-const HOST = window.location.origin.replace(/^http/, 'ws');
-
+let connectingPromise;
 let ws;
 
+const HOST = window.location.origin.replace(/^http/, 'ws');
+const RECONNECT_INTERVAL = 5000;
+const READY_STATES = {
+	CONNECTING: 0,
+	OPEN: 1,
+	CLOSING: 3,
+	CLOSED: 3,
+};
+
 export function send(data) {
-	if (!ws) throw new Error('No WebSocket!');
-	ws.send(JSON.stringify(data));
+	if (ws && ws.readyState === READY_STATES.OPEN) {
+		ws.send(JSON.stringify(data));
+	} else {
+		// eslint-disable-next-line no-use-before-define
+		start().then(() => send(data));
+	}
 }
 
 function onConnect(gameId) {
@@ -23,15 +36,39 @@ function onMessage({ data } = {}) {
 function onClose() {
 	console.log('closing connection'); // eslint-disable-line no-console
 
-	ws.removeEventListener('open', onConnect);
 	ws.removeEventListener('message', onMessage);
 	ws.removeEventListener('close', onClose);
+
+	ws = undefined;
+
+	// eslint-disable-next-line no-use-before-define
+	start();
 }
 
-export function start(gameId) {
-	ws = ws || new WebSocket(HOST);
+export function start(gameId = getGameId(store.getState())) {
+	connectingPromise = connectingPromise || new Promise((resolve, reject) => {
+		ws = new WebSocket(HOST);
 
-	ws.addEventListener('open', onConnect.bind(undefined, gameId));
-	ws.addEventListener('message', onMessage);
-	ws.addEventListener('close', onClose);
+		ws.addEventListener('open', resolve);
+		ws.addEventListener('error', reject);
+	}).then(() => {
+		// Web socket has connected
+		ws.addEventListener('message', onMessage);
+		ws.addEventListener('close', onClose);
+
+		connectingPromise = undefined;
+
+		onConnect(gameId);
+	}).catch(() => {
+		// Web socket failed to connect
+		console.log('reconnecting'); // eslint-disable-line no-console
+		ws = undefined;
+
+		return new Promise(resolve => setTimeout(resolve, RECONNECT_INTERVAL)).then(() => {
+			connectingPromise = undefined;
+			return start(gameId);
+		});
+	});
+
+	return connectingPromise;
 }
