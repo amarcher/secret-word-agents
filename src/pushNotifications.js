7fd555487802c3160bb0af7197f8ed6b931a8883
Andrew Archer <aarcher@slack-corp.com>
2018-03-11 22:27:59 -0700

    use another APN key

--- src/pushNotifications.js
+++ src/pushNotifications.js
@@ -7,7 +7,7 @@ class NotificationService {
 				token: {
 					key: process.env.APN_CERT,
 					keyId: '0D B3 8B 47 2F 20 65 2D AB D1 A5 64 9C 35 ED 09 A0 8A 16 FD',
-					teamId: 'J39B2498YF',
+					teamId: 'JRYHCAHRHF',
 				},
 			},
 			gcm: {
